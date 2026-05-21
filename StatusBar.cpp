477d2211016c0485f08d9313a8ccef25e6cca478
emiliaa27 <emiliasobala9@gmail.com>
2026-05-21 18:32:00 +0200

    Add files via upload

---
+++ StatusBar.cpp
@@ -0,0 +1,43 @@
+#include "StatusBar.h"
+#include <iostream>
+#include <iomanip>
+
+
+StatusBar::StatusBar(std::string name, float maxVal)
+    : name(name), currentValue(maxVal), maxValue(maxVal) {
+}
+
+void StatusBar::setValue(float newValue) {
+    if (newValue < 0) currentValue = 0;
+    else if (newValue > maxValue) currentValue = maxValue;
+    else currentValue = newValue;
+}
+
+float StatusBar::getValue() const {
+    return currentValue;
+}
+
+void StatusBar::stats() const {
+    std::cout << "\x1b[48;2;38;7;11m" << std::left << std::setw(7) << name << ": "
+        << std::right << std::setw(2) << (int)currentValue << "/"
+        << std::left << std::setw(3) << (int)maxValue << "\x1b[0m";
+}
+
+void StatusBar::draw() const {
+    std::cout << "\x1b[48;2;38;7;11m" << "\xE2\x99\xA5  [";
+    int full = (int)currentValue;
+    int max = (int)maxValue;
+
+    for (int i = 0; i < full; i++) std::cout << "\xE2\x96\x93";
+
+    int shadows = 0;
+    if (full > 0 && full < max) {
+        shadows = (full == max - 1) ? 1 : 2;
+        for (int i = 0; i < shadows; i++) std::cout << "\xE2\x96\x92";
+    }
+
+    int empty = max - full - shadows;
+    for (int i = 0; i < empty; i++) std::cout << "\xE2\x96\x91";
+
+    std::cout << "]" << "\x1b[0m";
+}
--- StatusBar.h
+++ StatusBar.h
@@ -1,52 +1,21 @@
 #ifndef STATUSBAR_H
 #define STATUSBAR_H
 
-#include <iostream>
 #include <string>
-#include <iomanip>
 
 class StatusBar {
+private:
     std::string name;
     float currentValue;
     float maxValue;
 
 public:
-    StatusBar(std::string name, float maxVal)
-        : name(name), currentValue(maxVal), maxValue(maxVal) {
-    }
+    StatusBar(std::string name, float maxVal);
 
-    void setValue(float newValue) {
-        if (newValue < 0) currentValue = 0;
-        else if (newValue > maxValue) currentValue = maxValue;
-        else currentValue = newValue;
-    }
-
-    float getValue() const { return currentValue; }
-
-    void stats() const {
-        std::cout << "\x1b[48;2;38;7;11m" << std::left << std::setw(7) << name << ": "
-            << std::right << std::setw(2) << (int)currentValue << "/"
-            << std::left << std::setw(3) << (int)maxValue << "\x1b[0m";
-    }
-
-    void draw() const {
-        std::cout << "\x1b[48;2;38;7;11m" << "\xE2\x99\xA5  [";
-        int full = (int)currentValue;
-        int max = (int)maxValue;
-
-        for (int i = 0; i < full; i++) std::cout << "\xE2\x96\x93";
-
-        int shadows = 0;
-        if (full > 0 && full < max) {
-            shadows = (full == max - 1) ? 1 : 2;
-            for (int i = 0; i < shadows; i++) std::cout << "\xE2\x96\x92";
-        }
-
-        int empty = max - full - shadows;
-        for (int i = 0; i < empty; i++) std::cout << "\xE2\x96\x91";
-
-        std::cout << "]" << "\x1b[0m";
-    }
+    void setValue(float newValue);
+    float getValue() const;
+    void stats() const;
+    void draw() const;
 };
 
-#endif
+#endif 
