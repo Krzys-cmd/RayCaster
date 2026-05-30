662c213f0a79832da326d0c50a18b1a02100a452
krzys-cmd <krzysiuglomb@gmail.com>
2026-05-30 16:39:12 +0200

    dodanue plikow

---
+++ grafika.h
@@ -0,0 +1,14 @@
+#ifndef GRAFIKA_H_INCLUDED
+#define GRAFIKA_H_INCLUDED
+
+
+class grafika {
+public:
+
+    float mapuj(float x, float in_min, float in_max, float out_min, float out_max);
+   // void bufor();
+    void BuforMapa();
+    void wypiszBufor();
+};
+
+#endif // GRAFIKA_H_INCLUDED
